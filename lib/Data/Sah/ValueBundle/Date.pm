d48bc49c42b3d29351e0cdabbe9a49c14f8212e1
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2023-03-30 08:09:36 +0700

    Initial commit

---
+++ lib/Data/Sah/ValueBundle/Date.pm
@@ -0,0 +1,17 @@
+package Data::Sah::ValueBundle::Date;
+
+use strict;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+1;
+# ABSTRACT: Default value rules related to dates (and times)
+
+=head1 SEE ALSO
+
+L<Data::Sah::DefaultValue>
+
+L<Data::Sah>
